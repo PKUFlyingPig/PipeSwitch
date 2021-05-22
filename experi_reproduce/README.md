6dff45e162e766c9d2be5bccf45e4d1b51eae733
Zhen Zhang <zhangzhen.dev@gmail.com>
2021-05-22 08:08:08 +0000

    setup script finished

--- experi_reproduce/README.md
+++ experi_reproduce/README.md
@@ -1,6 +1,15 @@
 # Reproduce experiment figures
 
-## Setup
+## AWS Setup
 
 All experiments are conducted on AWS. We use two EC2 instance types. One is p3.2xlarge, which is configured with 8 vCPUs (Intel Xeon E5-2686 v4), 1 GPU (NVIDIA V100 with 16 GB GPU memory), PCIe 3.0 ×16, and 61 GB memory. The other is g4dn.2xlarge, which is configured with 8 vCPUs (Intel Platinum 8259CL), 1 GPU (NVIDIA T4 with 16 GB GPU memory), PCIe 3.0 ×8, and 32 GB memory. The software environment includes PyTorch-1.3.0, torchvision- 0.4.2, scipy-1.3.2, and CUDA-10.1.
 
+## Pipeswich Setup 
+ssh into your (correctly configured) AWS server, you need to install anaconda first, then create a environment or just use the (base) environment.
+Then run the setup script which will install the pipeswitch pytorch\_plugin automatically.
+```
+source setup.sh
+```
+If the script completed successfully, you will find a new folder pytorch/ under current directory, this is exactly the pytorch compiled with pipeswitch plugin.
+
+## 
--- experi_reproduce/setup.sh
+++ experi_reproduce/setup.sh
@@ -1,16 +1,34 @@
-############################################
-# step 1 : build pytorch v1.3.0 from source
-############################################
+echo "========================================="
+echo "step 1 : build pytorch v1.3.0 from source"
+echo "========================================="
 conda install numpy ninja pyyaml mkl mkl-include setuptools cmake cffi typing
-# Add LAPACK support for the GPU if needed
-conda install -c pytorch magma-cuda101 # or [magma-cuda92 | magma-cuda100 ] depending on your cuda version
+conda install -c pytorch magma-cuda101 
 git clone --recursive https://github.com/pytorch/pytorch
 cd pytorch
 git checkout v1.3.0
-# if you are updating an existing checkout
 git submodule sync
 git submodule update --init --recursive
 export CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}
 python setup.py install
+cd ..
 
+echo "=================================================="
+echo "step 2 : Copy modified files to the PyTorch folder"
+echo "=================================================="
+cd ../pytorch_plugin
+bash overwrite.sh ../experi_reproduce/pytorch
+cd -
 
+echo "====================================="
+echo "step 3 : Compile the modified PyTorch"
+echo "====================================="
+cd pytorch
+python setup.py install
+cd -
+
+echo "==============================================="
+echo "step 4 : add the path to the repo to PYTHONPATH"
+echo "==============================================="
+cd ..
+export PYTHONPATH=$PWD:$PYTHONPATH
+cd -
